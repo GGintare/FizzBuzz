396165a5ca13d2f23201691b67ef1a46bbae9b20
Gintare <g.grigalauskyte@gmail.com>
2021-08-26 23:33:20 +0300

    fixed something

--- index.py
+++ index.py
@@ -4,6 +4,7 @@
 # Bonus tasks:
 # 0. Return Foo whenever number is divisible by 7. Print FizzBuzzFoo when number is divisible by 3, 5 and 7.
 # 1. Make a results array instead of printing to console.
+#    1.1 Make a function
 # 2. Allow user to input any range.
 # 3. Accept range bounds as command line argument.
 # 4. Extract only numbers from the array and sort them.
@@ -11,16 +12,24 @@
 # 6. From the provided excel sheet get only the lines with numbers from task 5.
 # 7. Return only * from the list you made in task 6.
 
-for x in range(1, 101):
-    if x % 3 == 0 and x % 5 == 0 and x % 7 == 0:
-        print('FizzBuzzFoo')
-    elif x % 3 == 0 and x % 5 == 0:
-        print('FizzBuzz')
-    elif x % 7 == 0:
-        print('Foo')
-    elif x % 5 == 0:
-        print('Buzz')
-    elif x % 3 == 0:
-        print('Fizz')
-    else:
-        print(x)
+
+def main_function():
+    result = []
+    for x in range(1, 101):
+        string = ""
+        if x % 3 == 0:
+            string = string + 'Fizz'
+        if x % 5 == 0:
+            string = string + 'Buzz'
+        if x % 7 == 0:
+            string = string + 'Foo'
+        
+        if string:
+            result.append(string)
+        else:
+            result.append(x)
+ 
+    return result
+
+print(main_function())
+
